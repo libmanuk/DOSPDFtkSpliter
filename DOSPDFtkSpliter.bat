76af95c14893af5f8ce057e104e965c58988c2d9
Eric C. Weig <eweig@email.uky.edu>
2019-03-05 11:48:22 -0500

    Update DOSPDFtkSpliter.bat

--- DOSPDFtkSpliter.bat
+++ DOSPDFtkSpliter.bat
@@ -3,9 +3,9 @@ title DOSPDFtkSpliter, Eric C. Weig 2019
 echo Using PDFtk for Windows to split a multipage PDF into separate files
 
 echo File to convert:
-for %%i in (*.tif) do echo %%i
+for %%i in (*.pdf) do echo %%i
 echo processing files, please wait...
-for %%i in (*.tif) do pdftk input.pdf burst output output_%02d.pdf
+for %%i in (*.pdf) do pdftk input.pdf burst output output_%02d.pdf
 echo.
 echo All done!  :)
 pause
